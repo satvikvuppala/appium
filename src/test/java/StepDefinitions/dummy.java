b46c3d23019c01f2454dd3f91eea8db49f446c37
satvikvuppala <133941100+satvikvuppala@users.noreply.github.com>
2023-07-26 16:55:38 +0530

    Update dummy.java

--- src/test/java/StepDefinitions/dummy.java
+++ src/test/java/StepDefinitions/dummy.java
@@ -1,3 +1,3 @@
 //	R52T204LBKN     device
 //	emulator-5554   device
-//	The above are the list of emulator devices udid number. This would be useful to identify the devices and connect to them using appium server.
+//	The above are the list of emulator devices udid number. This would be useful to identify the devices and connect 
